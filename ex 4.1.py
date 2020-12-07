5b5de2228bc887ba478983c2fd372031a10670e1
DeadMaxim <ilagubin920@gmail.com>
2020-12-07 19:18:45 +0400

    lesson 3 homework exercise 4

--- ex 4.1.py
+++ ex 4.1.py
@@ -6,7 +6,7 @@ def my_func(var_1, var_2):
 while True:
     x = float(input('Укажите вещественное положительное число: '))
     y = int(input('Укажите целое отрицательное число: '))
-    if x > 0 and y < 0:
+    if x >= 0 and y <= 0:
         print(f'Результат: {my_func(x, y)}')
         break
     else:
